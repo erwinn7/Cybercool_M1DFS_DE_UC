de346590c4838b0b10c9b6901746f32158212cec
ThomasSanna <thomassanna1e@gmail.com>
2026-02-11 09:37:01 +0100

    hardcode the api url

--- frontend/config.js
+++ frontend/config.js
@@ -1,12 +1,4 @@
 const AC_CONFIG = {
-    API_URL: (function () {
-        const hostname = window.location.hostname;
-        // Check for common local development hostnames
-        if (hostname === 'localhost' || hostname === '127.0.0.1') {
-            return 'http://127.0.0.1:8000';
-        } else {
-            // Production URL
-            return 'https://api-cybercool.onrender.com';
-        }
-    })()
+    // API_URL: 'http://127.0.0.1:8000' // Uncomment for local development if needed
+    API_URL: 'https://api-cybercool.onrender.com'
 };
